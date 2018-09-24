ee4d3fdc1708b4ae200beca89e012b24c71d76df
noolo113 <noolo113@gmail.com>
2018-09-25 01:45:25 +0300

    Added PrintArray function

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,6 @@
 #include <string>
 
 
-std::vector<int> BubbleSort(const std::vector<int>& arr) {
-	auto ret = arr;
-
-	if(arr.size() <= 1) {
-		return ret;
-	}
-	else {
-		for(size_t i = 0; i < ret.size() - 1; ++i) {
-			for(size_t j = 0; j < ret.size() - i - 1; ++j) {
-				if(ret[j] > ret[j + 1]) {
-					std::swap(ret[j], ret[j + 1]);
-				}
-			}
-		}
-		return ret;
-	}
-}
-
-
 void PrintArray(const std::string& message, const std::vector<int>& arr) {
 	std::cout << message << ": ";
 	std::copy(arr.begin(), arr.end(), std::ostream_iterator<int>{std::cout, " "});
@@ -48,5 +29,4 @@ int main() {
 	}
 
 	PrintArray("Initial array", arr);
-	PrintArray("Bubble sort", BubbleSort(arr));
 }
