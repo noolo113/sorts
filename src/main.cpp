de1282fc76d49a91c01a37b680cd8a1217dfc549
noolo113 <noolo113@gmail.com>
2018-09-25 01:55:37 +0300

    Implemented insertion sort

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,23 @@
 #include <iterator>
 #include <cstddef>
 #include <string>
+#include <type_traits>
+
+
+
+std::vector<int> InsertionSort(const std::vector<int>& arr) {
+	std::remove_const<std::remove_reference<decltype(arr)>::type>::type ret;
+	ret.push_back(arr[0]);
+	for(auto i = arr.begin() + 1; i != arr.end(); ++i) {
+		auto p = ret.begin();
+		for(; p != ret.end(); ++p) {
+			if(*p >= *i) break;
+		}
+		ret.insert(p, *i);
+	}
+	return ret;
+}
+
 
 
 void PrintArray(const std::string& message, const std::vector<int>& arr) {
@@ -29,4 +46,5 @@ int main() {
 	}
 
 	PrintArray("Initial array", arr);
+	PrintArray("Insertion sort", InsertionSort(arr));
 }
